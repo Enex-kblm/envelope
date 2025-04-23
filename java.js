d83f8c5a58f09e8873e3ab777529be673575546a
Enex-kblm <membventra@gmail.com>
2025-04-23 14:37:58 +0700

    Update java.js

--- java.js
+++ java.js
@@ -22,13 +22,13 @@ function updatePopupText() {
     const switchButton = document.querySelector('.popup-content button:nth-of-type(2)'); 
     
     if (currentLetter === 1) {
-        popupText.innerHTML = "Makasih ya bu udah ngajarin aku pelajaran informatika dari kelas 7. Jujur kalo belajar tik sama ibu itu asik ga bosenin hehehe.";
+        popupText.innerHTML = "TEXT";
         switchButton.style.display = 'inline-block'; 
     } else if (currentLetter === 2) {
-        popupText.innerHTML = "Tetap semangat ya bu di sekolah barunya. Aku doa in ibu makin sukses, aamiiin.";
+        popupText.innerHTML = "TEXT";
         switchButton.style.display = 'inline-block'; 
     } else {
-        popupText.innerHTML = 'Oh iya bu kalo boleh follow ig aku sama tt aku bu nanti aku follback ehehe. ig :  <a href="https://www.instagram.com/gtwuuyyy_?igsh=MWI1aWFmZDJlc25xNg==" target="_blank">@gtwuuyyy_</a> tt : <a href="https://www.tiktok.com/@ooone_1?_t=8pKM79CxpiB&_r=1" target="_blank">@ooone_1</a> kalo gamau gapapa sih, ga maksa.'; 
+        popupText.innerHTML = "TEXT"; 
         switchButton.style.display = 'none';
     }
 }
